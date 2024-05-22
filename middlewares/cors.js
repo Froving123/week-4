fa6e6680fe082ced861becc3a5c4a5ea2390c553
Froving123 <chernyh.sasha26@gmail.com>
2024-05-22 18:18:12 +0500

    1

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const allowedCors = [
   'https://students-projects.ru',
   'http://localhost:3000',
   'http://localhost:3001',
-  'https://froving-st.nomoredomainswork.ru/api'  
+  'https://froving-st.nomoredomainswork.ru'  
 ];
 
 function cors(req, res, next) {
