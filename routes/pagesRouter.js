ce70896207cd9dacfbdab0e07d483bfb51fd2522
Froving123 <chernyh.sasha26@gmail.com>
2024-05-26 18:24:43 +0500

    1

--- routes/pagesRouter.js
+++ routes/pagesRouter.js
@@ -2,7 +2,7 @@ const pagesRouter = require("express").Router();
 const { sendIndex, sendDashboard } = require("../controllers/auth.js");
 const { checkAuth, checkCookiesJWT } = require("../middlewares/auth.js");
 
-pagesRouter.get("/", checkCookiesJWT, checkAuth, sendIndex); 
+pagesRouter.get("/", sendIndex); 
 pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard); 
 
 module.exports = pagesRouter;
